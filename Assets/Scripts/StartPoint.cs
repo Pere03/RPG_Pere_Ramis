4575156b87e1736b0c36dcf7b8e649462a80e4dc
Pere03 <pereramis03@gmail.com>
2022-10-18 14:52:00 +0200

    rpg

---
+++ Assets/Scripts/StartPoint.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPoint : MonoBehaviour
+{
+    private PlayerController player;
+
+    [SerializeField] private Vector2 facingDirection;
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+
+        player.transform.position = transform.position;
+
+        player.lastDirection = facingDirection;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
